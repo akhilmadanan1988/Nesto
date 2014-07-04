a8482664a5d2ac8b1eb5aaf73f244acaa94cf3ef
Akhil <akhil.madanan@nlindia.com>
2014-07-04 15:29:43 +0530

    dsf

--- js/turn.js
+++ js/turn.js
@@ -5,6 +5,16 @@
  * Copyright (C) 2012, Emmanuel Garcia.
  * All rights reserved.
  *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Any redistribution, use, or modification is done solely for personal 
+ * benefit and not for any commercial purpose or for monetary gain.
+ * 
  **/
 
 (function($) {
@@ -19,7 +29,7 @@ var has3d,
 
 	A90 = PI/2,
 
-	isTouch = 'Touch' in window,
+	isTouch = 'ontouchstart' in window,
 
 	events = (isTouch) ? {start: 'touchstart', move: 'touchmove', end: 'touchend'}
 			: {start: 'mousedown', move: 'mousemove', end: 'mouseup'},
