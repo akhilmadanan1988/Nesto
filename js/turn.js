cc017e6ba908075e64de17b391acacd21d5e8d47
Akhil <akhil.madanan@nlindia.com>
2014-07-04 09:56:17 +0530

    new

--- js/turn.js
+++ js/turn.js
@@ -5,16 +5,6 @@
  * Copyright (C) 2012, Emmanuel Garcia.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *
- * 2. Any redistribution, use, or modification is done solely for personal 
- * benefit and not for any commercial purpose or for monetary gain.
- * 
  **/
 
 (function($) {
@@ -29,7 +19,7 @@ var has3d,
 
 	A90 = PI/2,
 
-	isTouch = 'ontouchstart' in window,
+	isTouch = 'Touch' in window,
 
 	events = (isTouch) ? {start: 'touchstart', move: 'touchmove', end: 'touchend'}
 			: {start: 'mousedown', move: 'mousemove', end: 'mouseup'},
