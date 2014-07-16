e091259608351e911aab74108dbfbbc81123e553
Akhil <akhil.madanan@nlindia.com>
2014-07-16 10:39:55 +0530

    latest

--- js/jsEvents.js
+++ js/jsEvents.js
@@ -5,9 +5,9 @@ $(function(){
 $(document).off('pagebeforeshow',all_pages).on('pagebeforeshow', all_pages, function myCallback (){
 	
   var _thisid = this.id; //this is the loading page id can be used to load scripts or write js/functions conditionally
+  $.getScript('js/ajaxCall.js');
   $.getScript('js/login.js');
-  
-
+  $.getScript('js/updateUser.js');
  })
  $('body').off('click','#checkbox').on('click','#checkbox',function(){
  	
