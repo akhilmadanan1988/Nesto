bdd686fda5bb18961a5c4890525bc8bdc0eb12f5
Akhil <akhil.madanan@nlindia.com>
2014-07-21 18:38:47 +0530

    latest

--- js/jsEvents.js
+++ js/jsEvents.js
@@ -3,20 +3,10 @@ $(function () {
 
     $(document).off('pageshow', all_pages).on('pageshow', all_pages, function myCallback() {
         var _thisid = this.id; //this is the loading page id can be used to load scripts or write js/functions conditionally
-        $.getScript('js/login.js');
-        if (_thisid === 'gallery') {
-            $.getScript('js/jquery-1.10.0.min.js', function () {
-                $.getScript('js/modernizr.min.js', function () {
-                    $.getScript('js/jquery.mousewheel.min.js', function () {
-                        $.getScript('js/jquery.hammer.min.js', function () {
-                            $.getScript('js/TweenMax.min.js', function () {
-                                $.getScript('js/TouchNSwipe.min.js');
-                            });
-                        });
-                    });
-                });
-            });
-        }
+        $.getScript('js/ajaxCall.js');
+		$.getScript('js/login.js');
+		$.getScript('js/updateUser.js');
+       
     });
 
     $('body').off('click', '#checkbox').on('click', '#checkbox', function () {
